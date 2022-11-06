7332c7662274c446bcfeefe8391e750a7a3a51a0
tonero5760 <uzkat.au@gmail.com>
2022-11-06 12:24:29 +0100

    Finished designning the add new task form

---
+++ app.js
@@ -0,0 +1,13 @@
+window.onload = function(e){
+
+    console.log("able");
+   const addTaskWindow =  document.querySelector('.add-task');
+
+   const btnAddTask = document.querySelector('.btn-addTask');
+
+   addTaskWindow.classList.add('hide-item'); 
+
+   btnAddTask.addEventListener('click',function(e){
+    addTaskWindow.classList.toggle('hide-item');
+   })
+}
